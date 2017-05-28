0299f87527f757f4a3dc9db676d2a3e4de5efdd7
sepharg <danielperez.arg@gmail.com>
2017-05-28 17:50:10 +0100

    added networked bullet

---
+++ Unet/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMove>();
+        if (hitPlayer != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Unet/Assets/Scripts/PlayerMove.cs
+++ Unet/Assets/Scripts/PlayerMove.cs
@@ -5,9 +5,9 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour
 {
-
-	// Use this for initialization
-	void Start () {
+    public GameObject bulletPrefab;
+    // Use this for initialization
+    void Start () {
 		
 	}
 
@@ -22,9 +22,31 @@ public class PlayerMove : NetworkBehaviour
         var z = Input.GetAxis("Vertical") * 0.1f;
 
         transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
     }
     public override void OnStartLocalPlayer()
     {
         GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // This [Command] code is run on the server!
+
+        // create the bullet object locally
+        var bullet = (GameObject)Instantiate(bulletPrefab, transform.position - transform.forward, Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        // spawn the bullet on the clients
+        NetworkServer.Spawn(bullet);
+
+        // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
+        Destroy(bullet, 2.0f);
     }
 }
